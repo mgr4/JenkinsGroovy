482c14cf63f015a059cea6ff64a293cd198fdfa9
suvsap <suv@EPINBANW01B8.localdomain>
2022-08-05 19:49:12 +0530

    Modified releasenotes script

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,6 +1,8 @@
 import java.text.SimpleDateFormat
 import java.util;
 
+@NonCPS
+
     def now = new Date();
     def fmt = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
     echo "Date and Time IS: " + fmt.format(date);
