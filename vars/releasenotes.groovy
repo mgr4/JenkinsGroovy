4a02b213f00347a7fef2cafe80e63d222b03ddff
suvsap <suv@EPINBANW01B8.localdomain>
2022-08-08 13:01:49 +0530

    Added script release notes

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,5 +1,1 @@
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println("This script is releasenotes which was defined as part of Global shared library");
-    }
-}
+println("This script is releasenotes which was defined as part of Global shared library");
