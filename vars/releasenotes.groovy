b245b853087c6325e11154c70f9f22eeb97cd9b1
suvsap <suv@EPINBANW01B8.localdomain>
2022-08-08 18:18:48 +0530

    Reverted releasenotes grrovy

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,7 +1,20 @@
-import groovy.lang.*
-import jenkins.*
-import jenkins.model.*
-import hudson.*
-import hudson.model.*
-println("This is test")
-// println ("This is test")
+import java.io.*
+import groovy.io.*
+
+def call(Map config=[:]){
+def dir = new File(pwd())
+
+new File(dir.path + '/releasenotes.txt').withWriter('utf-8')
+        {
+                writer ->
+            dir.eachFileRecurse(FileType.ANY) { file ->
+         if (file.isDirectory()){
+                    writer.writeLine(file.name);
+                }
+                else
+                {
+                    writer.writeLine('\t' + file.name + '\t' + file.length());
+                }
+    }
+  }
+}
