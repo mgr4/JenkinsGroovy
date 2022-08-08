d029045f709dcdeb0b9ce042d87b93ac65d905b5
suvsap <suv@EPINBANW01B8.localdomain>
2022-08-08 13:07:58 +0530

    Modified releasenotes groovy

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,1 +1,20 @@
-println("This script is releasenotes which was defined as part of Global shared library");
+import java.io.*
+import groovy.io.*
+
+def call(Map config=[:]){
+def dir = new File("C:\\code\\JenkinsGroovy\\ConsoleApp1")
+
+new File(dir.path + '/releasenotes.txt').withWriter('utf-8') 
+	{ 
+		writer -> 
+	    dir.eachFileRecurse(FileType.ANY) { file ->
+         if (file.isDirectory()){
+		    writer.writeLine(file.name);            
+		}
+		else
+		{
+		    writer.writeLine('\t' + file.name + '\t' + file.length());
+		}
+    }
+  }
+}
