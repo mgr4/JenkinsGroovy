13547825aca4aa46d742c20360dae9f9aa636ece
suvsap <suv@EPINBANW01B8.localdomain>
2022-08-08 16:53:50 +0530

    Test 3

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,2 +1,7 @@
 import groovy.lang.*
-println ("This is test")
+import jenkins.*
+import jenkins.model.*
+import hudson.*
+import hudson.model.*
+println(Jenkins.instance.pluginManager.plugins)
+// println ("This is test")
