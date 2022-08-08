be7d11e371104806e5c121810a74fe7b4aebb851
suvsap <suv@EPINBANW01B8.localdomain>
2022-08-08 14:01:48 +0530

    Simple script

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,20 +1,5 @@
-class User{
-String ln;
-String fn
-
-public String Username(){
-return getUserName(this.fn,this.ln)
-}
-
-private String getUserName(String fn, String ln){
+String getUserName(String fn, String ln){
 return fn.substring(0,1).toLowerCase() + ln.toLowerCase()
 }
-}
 
-
-String[] fn = ["Bob","Julie","Lisa"]
-String[] ln = ["Dylan","Lyne","Petty"]
-for (int i=0; i<fn.size(); i++){
-User u=new User(fn: fn[i], ln: ln[i])
-println("Username is ${u.Username()}")
-}
+println getUserName("Chris", "Behrens")
